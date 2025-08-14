c5b20d8f1af4aad7f4211d23ec08c5735707f039
jj-9901 <178759356+jj-9901@users.noreply.github.com>
2025-08-14 14:51:16 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZLMhRxSFYqrr9f1asfvgC9LRfHfkAGOM",
+  authDomain: "words-ce64f.firebaseapp.com",
+  projectId: "words-ce64f",
+  storageBucket: "words-ce64f.firebasestorage.app",
+  messagingSenderId: "538364615373",
+  appId: "1:538364615373:web:81b8b56f2385e489a5e784",
+  measurementId: "G-TW6YTTGZHM"
+};
